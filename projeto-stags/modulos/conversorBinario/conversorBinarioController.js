e72659f2909bdd508e39f2baad111c686964968f
stgomess <90339107+stgomess@users.noreply.github.com>
2022-07-25 10:44:49 -0300

    Delete projeto-stags directory

--- projeto-stags/modulos/conversorBinario/conversorBinarioController.js
+++
@@ -1,9 +0,0 @@
-function converteDecimal() {
-    var decimalPreenchido = document.getElementById("decimal").value;
-    if(decimalPreenchido) {
-        decimalPreenchido = parseInt(decimalPreenchido).toString(2);
-        document.getElementById("binary").value = decimalPreenchido;
-    } else {
-        document.getElementById("binary").value = "Preencha o Campo!";
-    } 
-}
--- projeto-stags/modulos/trocaDestroca/trocaDestrocaController.js
+++
@@ -1,7 +0,0 @@
-function trocar() {
-    var troca = document.getElementById("troca").value;
-    var destroca = document.getElementById("destroca").value;
-
-    document.getElementById("troca").value = destroca;
-    document.getElementById("destroca").value = troca;
-}
